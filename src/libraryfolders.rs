85c8db40abca86fb9a0b7a66c933bdf7413209b1
d10sfan <d10sfan@gmail.com>
2021-09-27 10:12:48 -0500

    ?

--- src/libraryfolders.rs
+++ src/libraryfolders.rs
@@ -1,7 +1,5 @@
 use std::path::PathBuf;
 use keyvalues_parser::Vdf;
-use std::io;
-use std::io::{Error, ErrorKind};
 
 /// An instance which contains all the Steam library folders installed on the file system.
 /// Example:
@@ -32,14 +30,14 @@ pub struct LibraryFolders {
 }
 
 impl LibraryFolders {
-	pub(crate) fn discover(&mut self, path: &PathBuf) -> io::Result<()> {
+	pub(crate) fn discover(&mut self, path: &PathBuf) {
 		let mut steamapps_name = "SteamApps";
 		
-		let mut steamapps = path.join(steamapps_name);
+		let mut steamapps = path.join(&steamapps_name);
 		
 		if !steamapps.is_dir() {
 			steamapps_name = "steamapps";
-			steamapps = path.join(steamapps_name);
+			steamapps = path.join(&steamapps_name);
 		}
 		
 		self.paths.push(steamapps.clone());
@@ -50,19 +48,28 @@ impl LibraryFolders {
 			let vdf_text = match std::fs::read_to_string(libraryfolders_vdf_path) {
 				Ok(s) => s,
 				Err(err) => {
-					 return Err(err);
+					 return;
 				}
 			};
 			let vdf = match Vdf::parse(&vdf_text) {
 				Ok(s) => s,
 				Err(err) => {
-					 return Err(Error::new(ErrorKind::Other, "parsing vdf text failed"));
+					 return;
 				}
 			};
 			
 			let vdf_key = vdf.key;
 			for (name, obj) in vdf.value.unwrap_obj().iter() {
 				println!("{} is {:?}", name, obj);
+				
+				let data = &obj[0];
+				println!("data {:?}", data);
+				
+				/*println!("found path: {:?}", path);
+				
+				let pathbuf = PathBuf::from(path);
+				let pathbuf_joined = pathbuf.join(&steamapps_name);
+				self.paths.append(pathbuf_joined);*/
 			}
 			
 			
@@ -70,6 +77,5 @@ impl LibraryFolders {
 		}
 		
 		self.discovered = true;
-		Ok(())
 	}
 }
